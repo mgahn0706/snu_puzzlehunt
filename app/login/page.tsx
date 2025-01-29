5bcbd20e61b4d6b649b780a37ebe5b10378fa16d
안민규 <mgahn0706@snu.ac.kr>
2025-01-30 00:39:33 +0900

    fix: redirect to home if authenticated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,16 @@
+import { useSession } from "next-auth/react";
 import { LoginForm } from "../components/login/LoginForm";
+import { useRouter } from "next/navigation";
 
 export default function SignIn() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "authenticated") {
+    router.push("/");
+    return;
+  }
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
